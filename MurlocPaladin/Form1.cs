290fd6b5764c01b03b36aa2d92d8f9fcf77c8249
SA1992X <825983081@qq.com>
2016-04-13 21:15:23 +0800

    12

--- MurlocPaladin/Form1.cs
+++ MurlocPaladin/Form1.cs
@@ -50,7 +50,7 @@ namespace MurlocPaladin
             }
             else
             {
-                MessageBox.Show("鱼人数目不对啊，你逗我！");
+                MessageBox.Show("鱼人数目是错的");
             }
 
 
